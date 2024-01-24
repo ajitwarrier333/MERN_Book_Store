9f41cde1921fa22c2c6cbc2b5d224bba649f207d
Ajit Warrier <ajith.warrier2000@gmail.com>
2024-01-24 13:55:28 +0530

    Creating HTTP Route and adding MongoDB and Mongoose to node js

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,6 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://MERNBookStoreDemo:Ny14vQ89MtiAY5CE@book-store-mern.ssnaybr.mongodb.net/books-collection?retryWrites=true&w=majority'
+
+// Please create a free database for yourself.
+// This database will be deleted after the tutorial.
